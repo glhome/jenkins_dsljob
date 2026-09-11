adaeb5fee39f8f35aaeb6c05718367362f4a59ca
glhome <glhome@yahoo.com>
2026-09-10 19:18:33 -0700

    name fix

--- jenkins-infra/jobs/utilities/windows_image.groovy
+++ jenkins-infra/jobs/utilities/windows_image.groovy
@@ -1,28 +1,29 @@
 folder('utilities')
 
 pipelineJob('utilities/windows-image') {
-
     description('Builds a serviced Windows ISO by applying SSU and LCU updates to a base Windows image.')
+    disableConcurrentBuilds()
+
+    buildDiscarder {
+        logRotator {
+            daysToKeep(30)
+            numToKeep(20)
+            artifactDaysToKeep(30)
+            artifactNumToKeep(10)
+        }
+    }
 
     parameters {
         stringParam('BASE_ISO_URL', '', 'URL of the base Windows ISO')
         stringParam('BASE_ISO_SHA256', '', 'Optional SHA-256 checksum for the base ISO')
-
         stringParam('SSU_URL', '', 'URL of the Servicing Stack Update (.msu)')
         stringParam('SSU_SHA256', '', 'Optional SHA-256 checksum for the SSU')
-
         stringParam('LCU_URL', '', 'URL of the Latest Cumulative Update (.msu)')
         stringParam('LCU_SHA256', '', 'Optional SHA-256 checksum for the LCU')
-
         stringParam('IMAGE_INDEX', '1', 'install.wim image index to service')
         stringParam('OUTPUT_NAME', 'Windows-Custom', 'Output ISO file name without extension')
         stringParam('AGENT_LABEL', 'windows-image-builder', 'Jenkins agent label')
-
-        booleanParam(
-            'KEEP_WORKSPACE',
-            false,
-            'Keep the image workspace after the build'
-        )
+        booleanParam('KEEP_WORKSPACE', false, 'Keep the image workspace after the build')
     }
 
     definition {
