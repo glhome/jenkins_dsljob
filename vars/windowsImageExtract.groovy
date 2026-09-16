ec3cb80e938c7d793b45621936d8cab7d9b36344
glhome <glhome@yahoo.com>
2026-09-15 21:10:27 -0700

    fix

--- vars/windowsImageExtract.groovy
+++ vars/windowsImageExtract.groovy
@@ -1,8 +1,33 @@
 def call(Map cfg = [:]) {
+
     def workRoot = cfg.workRoot
-    if (!workRoot) error 'workRoot is required'
-    def script = libraryResource('scripts/windows-image/extract-iso.ps1')
-    def scriptPath = "${env.WORKSPACE}/extract-windows-image.ps1"
-    writeFile file: scriptPath, text: script
-    powershell("& '${scriptPath}' -WorkRoot '${workRoot}' -ImageIndex ${cfg.imageIndex ?: 1}")
+    def imageIndex = cfg.imageIndex ?: 1
+
+    if (!workRoot?.trim()) {
+        error 'workRoot is required'
+    }
+
+    def script = libraryResource(
+        'scripts/windows-image/extract-iso.ps1'
+    )
+
+    def scriptPath = "${env.WORKSPACE}\\extract-windows-image.ps1"
+
+    writeFile(
+        file: scriptPath,
+        text: script
+    )
+
+    powershell(
+        '''
+$ErrorActionPreference = 'Stop'
+
+& '__SCRIPT_PATH__' `
+    -WorkRoot '__WORK_ROOT__' `
+    -ImageIndex __IMAGE_INDEX__
+'''
+        .replace('__SCRIPT_PATH__', scriptPath)
+        .replace('__WORK_ROOT__', workRoot)
+        .replace('__IMAGE_INDEX__', imageIndex.toString())
+    )
 }
